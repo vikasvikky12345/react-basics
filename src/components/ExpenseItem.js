2c855c9cc38c8439a54296df44c74ec0cafad8ca
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-05-06 18:15:51 +0530

    Expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h1>Expense Item</h1>
+            <h3>Food Rs 10</h3>
+            <h3>Petrol Rs 100</h3>
+            <h3>Movies Rs 200</h3>
+        </div>
+    )
+}
+export default ExpenseItem;
