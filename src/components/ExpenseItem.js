e6352d37ed0b91c9bcd6b77aa7af54008bd842f8
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-05-06 19:34:48 +0530

    css and locationofexpenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
+    const expensedate = new Date(2021, 2, 28);
+    const expensetitle = 'carinsurance';
+    const expenseamount = 250;
+    const locationofexpenditure = 'Insurance'
     return (
-        <div>
-            <h1>Expense Item</h1>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3>
+        <div className="expense-item">
+            <div>date:{expensedate}</div>
+            <div className="expense-item__description">
+                <h2>title:{expensetitle}</h2>
+                <div className="expense-item__price">amount:{expenseamount}</div>
+            </div>
+            <div>location:{locationofexpenditure}</div>
         </div>
-    )
+    );
 }
 export default ExpenseItem;
