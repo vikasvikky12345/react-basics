e72bc7a7ff1535ea3e23de21545f10494fa3c85f
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-05-15 16:17:47 +0530

    added changeexpense buttom

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,10 +3,24 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem = (prop) => {
+    const clickHandler = () => {
+        console.log('clicked!!')
+    }
+    const deleteitem = () =>{
+        const item = document.getElementById(prop.id);
+        if(item && item.parentNode){
+            item.parentNode.removeChild(item);
+        }
+    };
+
+
+    
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={prop.date} />
+        <Card className="expense-item" id={prop.id}>
+            <ExpenseDate date={prop.date} />    
             <ExpenseDetails amount={prop.amount}  expenditure={prop.expenditure}   title={prop.title} />
+            <button onClick={clickHandler}>Change expense</button>
+            <button onClick={deleteitem}>delete expense</button>
         </Card>
     )
 }
