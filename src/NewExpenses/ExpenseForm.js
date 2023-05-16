80d36bbf1f0f0002a483cbaa2cb91382adb26e85
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-05-16 11:54:12 +0530

    useState for data

--- src/NewExpenses/ExpenseForm.js
+++ src/NewExpenses/ExpenseForm.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseForm.css'
 const ExpenseForm = () =>{
+    const[eneterdTitle,seteneteredTitle] = useState('')
+    const[eneterdAmount,seteneteredAmount] = useState('')
+    const[eneterdDate,seteneteredDate] = useState('')
+    
     const titleChangeHandler = (event)=>{
-        console.log(event.target.value)
+        seteneteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event)=>{
+        seteneteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event)=>{
+        seteneteredDate(event.target.value)
     }
     return (
         <form>
@@ -13,11 +23,11 @@ const ExpenseForm = () =>{
                 </div>
                 <div className='new-expense__control'>
                     <label>Expense Amount:</label>
-                    <input type='number' min='0.01' step='0.01'/>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date:</label>
-                    <input type='date' min='2022-01-01' max='2023-06-30' />
+                    <input type='date' min='2022-01-01' max='2023-06-30' onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__action'>
